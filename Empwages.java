823a905d95c762fcb748c847ad308c76f4937c90
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-20 23:21:30 +0530

    Added: EmpWages using SwitchCase

--- Empwages.java
+++ Empwages.java
@@ -1,37 +1,46 @@
-package org.emp;
+package org.test;
 
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
-//Employee PartTime Wages
+//partTime Employee Wages Using Switch_Case
 
-public class EmpPartTime {
+public class EmpPartTimeScase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner in = new Scanner(System.in);
 		int isFullTime = 1;
 		int isPartTime = 0;
 		int perHour = 20;
 		int FullDayHour = 8;
+		int day;
 		Random rn=new Random();
 		int rand=rn.nextInt(3);
-
-		if (rand == isPartTime) {
-			System.out.println("-------PartTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			int day = in.nextInt();
-			int partTimeWages = (day * (FullDayHour / 2)) * perHour;
-			System.out.println("PartTime_Wages_for " + day + " days : " + partTimeWages + " Rs.");
-
-		} else if (rand == isFullTime) {
+		
+		switch(rand) {
+		
+		case 1: 
 			System.out.println("-------FullTime_Employee_Wages-------");
 			System.out.println("Enter no Days Employee Worked : ");
-			int day = in.nextInt();
+			day = in.nextInt();
 			int FullTimeWages = perHour * (FullDayHour * day);
-			System.out.println("PartTime_Wages_for " + day + " days : S" + FullTimeWages + " Rs.");
-		} else
-			System.out.println("Employee Absent..!!!!");
-
+			System.out.println("Full_Time_Wages_for " + day + " days : S" + FullTimeWages + " Rs.");
+			break;
+		case 2:
+			System.out.println("-------PartTime_Employee_Wages-------");
+			System.out.println("Enter no Days Employee Worked : ");
+			day = in.nextInt();
+			int partTimeWages = (day * (FullDayHour / 2)) * perHour;
+			System.out.println("PartTime_Wages_for " + day + " days : " + partTimeWages + " Rs.");
+		break;
+		default:
+			System.out.println("Employee is Absent..!!!!");
+			break;
+					
+		}
+				
 	}
 
 }
