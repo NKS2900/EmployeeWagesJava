4d1ba7b24f48a8985f63421e1c9d51e4e8bdc25c
unknown <nksayyad2900.com>
2020-10-20 23:00:33 +0530

    Added: Calculate Employee Daily Wages.

--- Empwages.java
+++ Empwages.java
@@ -1,34 +1,25 @@
 package org.emp;
 
 import java.util.Random;
-import java.util.Scanner;
 
-public class EmpDailyWages {
-	
-	//Calculate Employee Daily Wages
+public class EmpCheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int isFullTime = 1;
-		int perHour = 20;
-		int FullDayHour = 8;
-		Scanner in = new Scanner(System.in);
-		System.out.println("-------Employee_Wages--------");
-
+		
+		//Check Employee is present or not Using Random()
+		
+		int isFullTime=1;
 		Random rn=new Random();
 		int rand=rn.nextInt(2);
-
-		if (rand == isFullTime) {
-
-			System.out.println("Enter no Days Employee Worked : ");
-			int day = in.nextInt();
-			int TotalWages = perHour * (FullDayHour * day);
-			System.out.println("Total_Salary for " + day + " days : " + TotalWages + " Rs.");
-		} 
+		System.out.println(rand);
+		System.out.println("-------Employee_Wages--------");
+		if(rand==isFullTime) {
+			System.out.println("Employee present");
+		}
 		else
-			System.out.println("Employee is absent..!!!");
+			System.out.println("Empoyee absent.");
 		
-		in.close();
 	}
 
 }
