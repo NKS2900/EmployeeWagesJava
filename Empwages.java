986ef78977bbe9f93c97ab53fc731b9dde883677
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-20 23:26:20 +0530

    Added: CalWages Till on Conditions

--- Empwages.java
+++ Empwages.java
@@ -1,42 +1,61 @@
-package org.test;
+package org.emp;
 
 import java.util.Random;
 
-public class EmpWagesMonthly {
+public class EmpWagesTill {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Calculate Employee Wages Monthly.
-		
-		int isFullTime = 1;
+//Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours
+			
+		int fullTimeDays = 0;
+		int partTimeDays = 0;
 		int perHour = 20;
 		int FullDayHour = 8;
 		int WorkingDayMonth = 20;
+		int maxHoursInMonth = 100;
+		int totalempHour = 0;
+		int totalWorkingDay = 0;
+		int absent = 0;
+		int i = 0;
 
-		System.out.println("-------Employee_Wages--------");
+		while (maxHoursInMonth > totalempHour && WorkingDayMonth > totalWorkingDay) {
 
-		Random rn=new Random();
-		int rand=rn.nextInt(3);
+			++totalWorkingDay;
+			Random rn = new Random();
+			int rand = rn.nextInt(3);
 
-		switch (rand) {
+			switch (rand) {
 
-		case 1:
-			System.out.println("-------FullTime_Employee_Wages_Monthly-------");
-			int FullTimeWages = perHour * (FullDayHour * WorkingDayMonth);
-			System.out.println("Full_Time_Wages_for " + WorkingDayMonth + " days : " + FullTimeWages + " Rs.");
-			break;
-		case 2:
-			System.out.println("-------PartTime_Employee_Wages_Monthly-------");
-			int partTimeWages = (WorkingDayMonth * (FullDayHour / 2)) * perHour;
-			System.out.println("PartTime_Wages_for " + WorkingDayMonth + " days : " + partTimeWages + " Rs.");
-			break;
-		default:
-			System.out.println("Employee is Absent..!!!!");
-			break;
+			case 0:
+				fullTimeDays++;
+				totalempHour = totalempHour + FullDayHour;
+				break;
+			case 1:
+				partTimeDays++;
+				totalempHour = totalempHour + (FullDayHour / 2);
+				break;
+			default:
+				absent++;
+				break;
 
+			}
 		}
+		int FullTimeSalary = perHour * (FullDayHour * fullTimeDays);
+		int PartTimeSalary = (partTimeDays * (FullDayHour / 2)) * perHour;
+
+		System.out.println("------------Employee_Wages-------------");
+		System.out.println("---------------------------------------");
+		System.out.println("FullTime_Days_in_month: " + fullTimeDays);
+		System.out.println("PartTime_Days_in_month: " + partTimeDays);
+		System.out.println("Absent_Days_in_month: " + absent);
+		System.out.println("Total_working_Days_IN_month : " + (fullTimeDays + partTimeDays));
+		System.out.println("Total_Hours    :===> " + totalempHour);
+		System.out.println("FullTime_Salary:===> " + FullTimeSalary);
+		System.out.println("PartTime_Salary:===> " + PartTimeSalary);
+		System.out.println("Total_Salary   :===> " + (FullTimeSalary + PartTimeSalary));
+		System.out.println("---------------------------------------");
 
 	}
-
 }
