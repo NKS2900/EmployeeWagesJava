e8b9a808405e46e12d3cf71a34d17d5d10bb462c
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-23 12:56:26 +0530

    Added:  EmpWages Use Class Method Class Variable

--- Empwages.java
+++ Empwages.java
@@ -3,44 +3,37 @@ package org.test;
 import java.util.Random;
 import java.util.Scanner;
 
-//partTime Employee Wages Using Switch_Case
+public class Empwages {
 
-public class EmpPartTimeScase {
+	public static final int isFullTime = 1;
+	public static final int isPartTime = 0;
+	public static final int perHour = 20;
+	public static final int FullDayHour = 8;
+	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("---------Employee_Wages-----------");
 		
-		Scanner in = new Scanner(System.in);
-		int isFullTime = 1;
-		int isPartTime = 0;
-		int perHour = 20;
-		int FullDayHour = 8;
-		int day;
-		Random rn=new Random();
-		int rand=rn.nextInt(3);
-		
-		switch(rand) {
-		
-		case 1: 
-			System.out.println("-------FullTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			day = in.nextInt();
-			int FullTimeWages = perHour * (FullDayHour * day);
-			System.out.println("Full_Time_Wages_for " + day + " days : S" + FullTimeWages + " Rs.");
-			break;
-		case 2:
-			System.out.println("-------PartTime_Employee_Wages-------");
-			System.out.println("Enter no Days Employee Worked : ");
-			day = in.nextInt();
-			int partTimeWages = (day * (FullDayHour / 2)) * perHour;
-			System.out.println("PartTime_Wages_for " + day + " days : " + partTimeWages + " Rs.");
-		break;
-		default:
-			System.out.println("Employee is Absent..!!!!");
-			break;
-					
-		}
-				
+		Empwages emp = new Empwages();
+		emp.CalEmpWages();
 	}
 
+	public void CalEmpWages() {
+		Random rn = new Random();
+		int rand = rn.nextInt(2);
+		if (rand == isFullTime) {
+			System.out.println("Enter fullTimeDays Employee Worked : ");
+			int day = in.nextInt();
+			int TotalWages = perHour * (FullDayHour * day);
+			System.out.println("Total_Salary for " + day + " days : " + TotalWages + " Rs.");
+		} 
+		else if(rand == isPartTime) {
+			System.out.println("Enter partTimeDays Employee Worked : ");
+			int day = in.nextInt();
+			int TotalWages = perHour * ((FullDayHour/2) * day);
+			System.out.println("Total_Salary for " + day + " days : " + TotalWages + " Rs.");
+		}else
+			System.out.println("Employee is absent..!!!");
+		in.close();
+	}
 }
