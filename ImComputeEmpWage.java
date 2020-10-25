f791689a3545929aab5494957892695d3eef4322
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-25 19:26:42 +0530

    Delete ImComputeEmpWage.java

--- ImComputeEmpWage.java
+++
@@ -1,8 +0,0 @@
-package UC_11;
-
-public interface ImComputeEmpWage
-{
-	public void addCompanyEmpWage(String company , int empRatePerHour , int numOfWorkingDays , int mxHoursPerMonth);
-	public void computeEmpWage();
-	public int getTotalWage(String company);
-}
