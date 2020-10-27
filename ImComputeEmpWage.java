ed5f1e8c10a105caeb4b4ce8c29d37f856455eb0
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-27 23:23:31 +0530

    Added

--- ImComputeEmpWage.java
+++
@@ -1,8 +0,0 @@
-package UC_14;
-
-public interface ImComputeEmpWage
-{
-	public void addCompanyEmpWage(String company , int empRatePerHour , int numOfWorkingDays , int mxHoursPerMonth);
-	public void computeEmpWage();
-	public int getTotalWage(String company);
-}
